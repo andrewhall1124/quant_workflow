6dba454fdb91e8cc849cf3b15ece6bcb76121d6e
andrewhall1124 <andrewmartinhall2@gmail.com>
2024-07-10 12:23:45 -0600

    Merge step for historical data load

--- dags/alpaca_stock_daily.py
+++ dags/alpaca_stock_daily.py
@@ -40,60 +40,142 @@ trading_client = TradingClient(api_key,secret_key)
 )
 def alpaca_stock_daily():
 
-    with TaskGroup("preparation") as preparation:
+    with TaskGroup("get_all_assets") as get_all_assets:
 
-        create_assets_table = PostgresOperator(
-        task_id="create_assets_table",
-        postgres_conn_id="pg_database",
-        sql="sql/create_assets_table.sql",
-        )
+        with TaskGroup("setup") as setup:
 
-        create_temp_assets_table = PostgresOperator(
-            task_id="create_temp_assets_table",
+            create_assets_table = PostgresOperator(
+            task_id="create_assets_table",
             postgres_conn_id="pg_database",
-        sql="sql/create_temp_assets_table.sql",
-        )
-
-        [create_assets_table, create_temp_assets_table]   
-         
-    @task()
-    def extract():
-
-        # Get all assets
-        asset_request = GetAssetsRequest(
-            asset_class = AssetClass.US_EQUITY,
-        )
-
-        all_assets = trading_client.get_all_assets(asset_request)
-
-        all_assets = [vars(asset) for asset in all_assets] # Convert objects to dicts
-
-        df = pd.DataFrame(data=all_assets)
-
-        data_path = "assets.csv"
-
-        df.to_csv(data_path,index=False)
-
-        postgres_hook = PostgresHook(postgres_conn_id="pg_database")
-        conn = postgres_hook.get_conn()
-        cur = conn.cursor()
-        with open(data_path, "r") as file:
-            cur.copy_expert(
-                "COPY temp_assets FROM STDIN WITH CSV HEADER DELIMITER AS ',' QUOTE '\"'",
-                file,
+            sql="sql/create_assets_table.sql",
             )
-        conn.commit()
+
+            create_temp_assets_table = PostgresOperator(
+                task_id="create_temp_assets_table",
+                postgres_conn_id="pg_database",
+            sql="sql/create_temp_assets_table.sql",
+            )
+
+            [create_assets_table, create_temp_assets_table]   
+            
+        @task()
+        def extract():
+
+            # Get all assets
+            asset_request = GetAssetsRequest(
+                asset_class = AssetClass.US_EQUITY,
+            )
+
+            all_assets = trading_client.get_all_assets(asset_request)
+
+            all_assets = [vars(asset) for asset in all_assets] # Convert objects to dicts
+
+            df = pd.DataFrame(data=all_assets)
+
+            data_path = "assets.csv"
+
+            df.to_csv(data_path,index=False)
+
+            postgres_hook = PostgresHook(postgres_conn_id="pg_database")
+            conn = postgres_hook.get_conn()
+            cur = conn.cursor()
+            with open(data_path, "r") as file:
+                cur.copy_expert(
+                    "COPY TEMP_ASSETS FROM STDIN WITH CSV HEADER DELIMITER AS ',' QUOTE '\"'",
+                    file,
+                )
+            conn.commit()
 
 
-    load = PostgresOperator(
-        task_id="load",
-        postgres_conn_id="pg_database",
-    sql="sql/merge_temp_into_assets_table.sql",
-    )
+        load = PostgresOperator(
+            task_id="load",
+            postgres_conn_id="pg_database",
+        sql="sql/merge_temp_into_assets_table.sql",
+        )
 
-    preparation >> extract() >> load
+        setup >> extract() >> load
+    
+    with TaskGroup("get_historical_data") as get_historical_data:
+
+        with TaskGroup("setup") as setup:
+
+            create_assets_table = PostgresOperator(
+            task_id="create_historical_data_table",
+            postgres_conn_id="pg_database",
+            sql="sql/create_historical_data_table.sql",
+            )
+
+            create_temp_assets_table = PostgresOperator(
+                task_id="create_temp_historical_data_table",
+                postgres_conn_id="pg_database",
+            sql="sql/create_temp_historical_data_table.sql",
+            )
+
+            [create_assets_table, create_temp_assets_table]  
+
+        @task
+        def get_current_symbols():
+            query = """
+                SELECT DISTINCT SYMBOL FROM ASSETS WHERE STATUS = 'active' AND TRADABLE = TRUE AND FRACTIONABLE = TRUE AND SHORTABLE = TRUE;
+            """
+
+            postgres_hook = PostgresHook(postgres_conn_id="pg_database")
+            conn = postgres_hook.get_conn()
+            cur = conn.cursor()
+            cur.execute(query)            
+            symbols = [row[0] for row in cur.fetchall()]
+            conn.commit()
+            return symbols
 
 
 
+        @task
+        def extract(symbols, execution_date):
+            # Parameters
+            end = execution_date.subtract(days=1)
+            start = end.subtract(days=1)
+
+            # Get previous days market data
+            bars_request = StockBarsRequest(
+                symbol_or_symbols=symbols,
+                timeframe=TimeFrame(1,TimeFrameUnit.Day),
+                start=start,
+                end=end,
+                adjustment=Adjustment.ALL,
+            )
+
+            bars = data_client.get_stock_bars(bars_request)
+
+            df = bars.df
+
+            df = df.reset_index()
+
+            data_path = "historical_data.csv"
+
+            df.to_csv(data_path,index=False)
+
+            postgres_hook = PostgresHook(postgres_conn_id="pg_database")
+            conn = postgres_hook.get_conn()
+            cur = conn.cursor()
+            with open(data_path, "r") as file:
+                cur.copy_expert(
+                    "COPY TEMP_HISTORICAL_DATA FROM STDIN WITH CSV HEADER DELIMITER AS ',' QUOTE '\"'",
+                    file,
+                )
+            conn.commit()
+
+        load = PostgresOperator(
+            task_id="load",
+            postgres_conn_id="pg_database",
+            sql="sql/merge_temp_into_historical_data_table.sql",
+        )
+
+        
+        symbols = get_current_symbols()
+        
+        setup >> extract(symbols) >> load
+
+
+    get_all_assets >> get_historical_data
     
 alpaca_stock_daily()
